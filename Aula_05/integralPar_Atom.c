deb2879039017c21745f91ecfe3577f36f1e10f5
etvorellana <evalero@uesc.br>
2020-11-12 16:05:12 -0300

    Correção em integralPar_Atom.c

--- Aula_05/integralPar_Atom.c
+++ Aula_05/integralPar_Atom.c
@@ -11,7 +11,7 @@
 
 /* Como compilar
 
-gcc -o integralPar_Crit -Wall -fopenmp -O2 integralPar_Crit.c
+gcc -o integralPar_Atom -Wall -fopenmp -O2 integralPar_Atom.c
 
 */
 
@@ -82,44 +82,4 @@ double int_f(double a, double b){
   double res = (b*b*b)/3 + b;
   res -= ((a*a*a)/3 + a);
   return res;
-}
-
-double f (double x);
-
-int main(){
-
-    double start = omp_get_wtime();
-
-    //double integral, a= 0.0, b= 1.0, x, h;
-    double integral, a= 0.0, b= 1.0, h;
-    int n = 4096;
-    //int n = 2048;
-
-    h = (b - a)/n;
-    //integral = (f(a) + f(b))/2;
-    //x = a;
-    #pragma omp parallel
-    {   
-        double integral_loc = 0.0;
-        #pragma omp for
-        for(int i = 1; i < n ; i++){
-            //x += h;
-            integral_loc += f(a + i*h);
-        }
-        #pragma omp atomic
-        integral += integral_loc;
-    } 
-    integral += (f(a) + f(b))/2;
-    integral *= h;
-
-    printf("Estimativa com %d trapezios\n", n);
-    printf("Integral = %.6lf\n", integral);
-    double stop = omp_get_wtime();
-    printf("Tempo total: %.6lf\n", stop -start);
-    return 0;
-}
-
-//Sugestão um polinomio de grau 8
-double f(double x){
-    return x*x + 1.0;
 }
